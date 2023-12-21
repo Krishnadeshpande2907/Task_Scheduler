e61b107e66cdef9b77f402603387289d117418cb
Krishna Deshpande <kd721921@gmail.com>
2023-12-21 15:59:49 +0530

    Added repeatingReminders() [Not finished]

--- src/Reminder.java
+++ src/Reminder.java
@@ -1,9 +1,13 @@
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Reminder {
 
     public void remind(){
-//        System.out.println(taskTime);
         while (LocalDateTime.now().isBefore(TimeRelated.getUserTaskStartTime()) || LocalDateTime.now().isEqual(TimeRelated.getUserTaskStartTime())) {
             try {
                 Thread.sleep(10000); // Wait for 10 seconds
@@ -12,5 +16,26 @@ public class Reminder {
             }
         }
         System.out.println("It is time to do your task");
+    }
+
+    public void repeatingReminders(){
+        int numberOfTimes = Questions.getNumberOfTimes();
+        long interval = Questions.getInterval();
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+        Runnable reminderTask = () -> {
+            for (int i = 0; i < numberOfTimes; i++) {
+                System.out.println("Reminder to do your task: " + Questions.getTask());
+                try {
+                    Thread.sleep(interval * 60 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            executorService.shutdown();
+        };
+
+        long delay = Duration.between(LocalTime.now(), TimeRelated.getUserTaskStartTime()).toMillis();
+        executorService.scheduleAtFixedRate(reminderTask, delay, interval * 60, TimeUnit.SECONDS);
     }
 }
